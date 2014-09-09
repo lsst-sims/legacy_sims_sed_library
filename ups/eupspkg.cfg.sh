3bfefa39155958a3d5cfc220c29d3c0e3f7b12df
LSST Software Manager <lsstsw@lsst-dev.ncsa.illinois.edu>
2014-09-08 19:50:04 -0500

    added longer flat sed

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,7 +2,7 @@ install()
 {
     default_install
     cd $PREFIX
-    curl -O "http://lsst-web.ncsa.illinois.edu/~krughoff/data/seds_090414.tar.gz"
-    tar zxvf seds_090414.tar.gz
-    rm seds_090414.tar.gz
+    curl -O "http://lsst-web.ncsa.illinois.edu/~krughoff/data/seds_090814.tar.gz"
+    tar zxvf seds_090814.tar.gz
+    rm seds_090814.tar.gz
 }
