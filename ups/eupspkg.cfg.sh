1672b04028276a0c5de3896b40fbf87b548dcae3
Simon Krughoff <simon.krughoff@gmail.com>
2014-10-06 18:15:52 -0700

    Compressed M-dwarf flare light curves

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,7 +2,7 @@ install()
 {
     default_install
     cd $PREFIX
-    curl -O "http://lsst-web.ncsa.illinois.edu/~krughoff/data/seds_090814.tar.gz"
+    curl -O "http://lsst-web.ncsa.illinois.edu/~krughoff/data/seds_100614.tar.gz"
     tar zxvf seds_090814.tar.gz
     rm seds_090814.tar.gz
 }
