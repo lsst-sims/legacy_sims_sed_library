7b2c9abf7cf0a849fb839e1aadc0af41a62882d3
Simon Krughoff <simon.krughoff@gmail.com>
2014-10-06 18:19:47 -0700

    fixed typos in eupspkg.cfg.sh

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -3,6 +3,6 @@ install()
     default_install
     cd $PREFIX
     curl -O "http://lsst-web.ncsa.illinois.edu/~krughoff/data/seds_100614.tar.gz"
-    tar zxvf seds_090814.tar.gz
-    rm seds_090814.tar.gz
+    tar zxvf seds_100614.tar.gz
+    rm seds_100614.tar.gz
 }
