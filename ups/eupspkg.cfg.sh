c055262f65b4fce62bd9256a5a8112986ed5bc6c
Mario Juric <mjuric@lsst.org>
2014-12-07 23:51:09 -0800

    Run curl with -# switch so that lsst-build detects progress

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,7 +2,7 @@ install()
 {
     default_install
     cd $PREFIX
-    curl -O "http://lsst-web.ncsa.illinois.edu/~krughoff/data/seds_100614.tar.gz"
+    curl -# -O "http://lsst-web.ncsa.illinois.edu/~krughoff/data/seds_100614.tar.gz"
     tar zxvf seds_100614.tar.gz
     rm seds_100614.tar.gz
 }
