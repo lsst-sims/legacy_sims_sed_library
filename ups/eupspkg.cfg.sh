5b328a8624af10bcd6eeab6148384ea206a2de43
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-06-20 13:11:10 +0000

    Update NCSA web server URL and handle redirects.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,7 +2,7 @@ install()
 {
     default_install
     cd $PREFIX
-    curl -O "https://lsst-web.ncsa.illinois.edu/sim-data/sed_library/seds_170124.tar.gz"
+    curl -LO "https://lsst.ncsa.illinois.edu/sim-data/sed_library/seds_170124.tar.gz"
     tar zxvf seds_170124.tar.gz
     rm seds_170124.tar.gz
 }
